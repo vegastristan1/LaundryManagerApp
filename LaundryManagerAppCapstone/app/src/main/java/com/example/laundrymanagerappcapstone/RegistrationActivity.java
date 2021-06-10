0d73e4446a25b228f008525af52ecaef0c3ff250
gldee <gldeeako2@gmail.com>
2021-06-10 16:52:37 +0800

    LaundryManagerCapstone

---
+++ LaundryManagerAppCapstone/app/src/main/java/com/example/laundrymanagerappcapstone/RegistrationActivity.java
@@ -0,0 +1,341 @@
+package com.example.laundrymanagerappcapstone;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.laundrymanagerappcapstone.apiconnection.Api;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static android.view.View.GONE;
+
+public class RegistrationActivity extends AppCompatActivity {
+    private static final int CODE_GET_REQUEST = 1024;
+    private static final int CODE_POST_REQUEST = 1025;
+
+    EditText editTextStoreId, editTextStoreName, editTextStoreOwner, editTextStoreContactNumber, editTextStoreAddress, editTextStorePassword, editTextStoreEmail;
+    Button buttonRegister;
+    ProgressBar progressBar;
+    ListView listView;
+
+    List<com.example.laundrymanagerappcapstone.apiconnection.Manager>managerList;
+
+    boolean isUpdating = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        editTextStoreId = findViewById(R.id.editTextStoreId);
+        editTextStoreName = findViewById(R.id.editTextStoreName);
+        editTextStoreOwner = findViewById(R.id.editTextStoreOwner);
+        editTextStoreContactNumber = findViewById(R.id.editTextStoreContactNumber);
+        editTextStoreAddress = findViewById(R.id.editTextStoreAddress);
+        editTextStorePassword = findViewById(R.id.editTextStorePassword);
+        editTextStoreEmail = findViewById(R.id.editTextStoreEmail);
+        buttonRegister = findViewById(R.id.buttonRegister);
+
+        progressBar = (ProgressBar) findViewById(R.id.loading);
+        listView = (ListView) findViewById(R.id.listViewHeroes);
+
+        managerList = new ArrayList<>();
+
+        buttonRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createHero();
+            }
+        });
+        readHeroes();
+    }
+
+    private void createHero() {
+        String store_name = editTextStoreName.getText().toString().trim();
+        String store_owner = editTextStoreOwner.getText().toString().trim();
+        String store_contact_number = editTextStoreContactNumber.getText().toString().trim();
+        String store_address = editTextStoreAddress.getText().toString().trim();
+        String store_password = editTextStorePassword.getText().toString().trim();
+        String store_email = editTextStoreEmail.getText().toString().trim();
+
+        if (TextUtils.isEmpty(store_name)) {
+            editTextStoreName.setError("Please enter name");
+            editTextStoreName.requestFocus();
+            return;
+        }
+
+        if (TextUtils.isEmpty(store_owner)) {
+            editTextStoreOwner.setError("Please enter real name");
+            editTextStoreOwner.requestFocus();
+            return;
+        }
+
+        if (TextUtils.isEmpty(store_contact_number)) {
+            editTextStoreContactNumber.setError("Please enter name");
+            editTextStoreContactNumber.requestFocus();
+            return;
+        }
+
+        if (TextUtils.isEmpty(store_address)) {
+            editTextStoreAddress.setError("Please enter real name");
+            editTextStoreAddress.requestFocus();
+            return;
+        }
+
+        if (TextUtils.isEmpty(store_password)) {
+            editTextStorePassword.setError("Please enter name");
+            editTextStorePassword.requestFocus();
+            return;
+        }
+
+        if (TextUtils.isEmpty(store_email)) {
+            editTextStoreEmail.setError("Please enter real name");
+            editTextStoreEmail.requestFocus();
+            return;
+        }
+
+        HashMap<String, String> params = new HashMap<>();
+        params.put("store_name", store_name);
+        params.put("store_owner", store_owner);
+        params.put("store_contact_number", store_contact_number);
+        params.put("store_address", store_address);
+        params.put("store_password", store_password);
+        params.put("store_email", store_email);
+
+        com.example.laundrymanagerappcapstone.RegistrationActivity.PerformNetworkRequest request = new com.example.laundrymanagerappcapstone.RegistrationActivity.PerformNetworkRequest(Api.URL_CREATE_STORE_MANAGER, params, CODE_POST_REQUEST);
+        request.execute();
+    }
+
+
+    private void readHeroes() {
+        com.example.laundrymanagerappcapstone.RegistrationActivity.PerformNetworkRequest request = new com.example.laundrymanagerappcapstone.RegistrationActivity.PerformNetworkRequest(Api.URL_READ_STORE_MANAGERS, null, CODE_GET_REQUEST);
+        request.execute();
+    }
+
+    private void refreshHeroList(JSONArray managers) throws JSONException {
+        managerList.clear();
+
+        for (int i = 0; i < managers.length(); i++) {
+            JSONObject obj = managers.getJSONObject(i);
+            managerList.add(new com.example.laundrymanagerappcapstone.apiconnection.Manager(
+                    obj.getInt("id"),
+                    obj.getString("store_name"),
+                    obj.getString("store_owner"),
+                    obj.getString("store_contact_number"),
+                    obj.getString("store_address"),
+                    obj.getString("store_password"),
+                    obj.getString("store_email")
+            ));
+        }
+
+        com.example.laundrymanagerappcapstone.RegistrationActivity.ManagerAdapter adapter = new com.example.laundrymanagerappcapstone.RegistrationActivity.ManagerAdapter(managerList);
+        listView.setAdapter(adapter);
+    }
+
+    private class PerformNetworkRequest extends AsyncTask<Void, Void, String> {
+        String url;
+        HashMap<String, String> params;
+        int requestCode;
+
+        PerformNetworkRequest(String url, HashMap<String, String> params, int requestCode) {
+            this.url = url;
+            this.params = params;
+            this.requestCode = requestCode;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressBar.setVisibility(View.VISIBLE);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            progressBar.setVisibility(GONE);
+            try {
+                JSONObject object = new JSONObject(s);
+                if (!object.getBoolean("error")) {
+                    Toast.makeText(getApplicationContext(), object.getString("message"), Toast.LENGTH_SHORT).show();
+                    refreshHeroList(object.getJSONArray("storemanagers"));
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            com.example.laundrymanagerappcapstone.apiconnection.RequestHandler requestHandler = new com.example.laundrymanagerappcapstone.apiconnection.RequestHandler();
+
+            if (requestCode == CODE_POST_REQUEST)
+                return requestHandler.sendPostRequest(url, params);
+
+
+            if (requestCode == CODE_GET_REQUEST)
+                return requestHandler.sendGetRequest(url);
+
+            return null;
+        }
+    }
+
+    class ManagerAdapter extends ArrayAdapter<com.example.laundrymanagerappcapstone.apiconnection.Manager> {
+        List<com.example.laundrymanagerappcapstone.apiconnection.Manager> managerList;
+
+        public ManagerAdapter(List<com.example.laundrymanagerappcapstone.apiconnection.Manager> managerList) {
+            super(com.example.laundrymanagerappcapstone.RegistrationActivity.this, R.layout.layout_store_manager_list, managerList);
+            this.managerList = managerList;
+        }
+
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            LayoutInflater inflater = getLayoutInflater();
+            View listViewItem = inflater.inflate(R.layout.layout_store_manager_list, null, true);
+
+            TextView textViewName = listViewItem.findViewById(R.id.textViewName);
+
+            TextView textViewUpdate = listViewItem.findViewById(R.id.textViewUpdate);
+            TextView textViewDelete = listViewItem.findViewById(R.id.textViewDelete);
+
+            final com.example.laundrymanagerappcapstone.apiconnection.Manager manager = managerList.get(position);
+
+            textViewName.setText(manager.getStore_name());
+
+            textViewUpdate.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    isUpdating = true;
+                    //editTextHeroId.setText(String.valueOf(hero.getId()));
+                    //editTextName.setText(hero.getName());
+                    //editTextRealname.setText(hero.getRealname());
+                    //ratingBar.setRating(hero.getRating());
+                    //spinnerTeam.setSelection(((ArrayAdapter<String>) spinnerTeam.getAdapter()).getPosition(hero.getTeamaffiliation()));
+                    //buttonAddUpdate.setText("Update");
+                }
+            });
+
+            textViewDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(com.example.laundrymanagerappcapstone.RegistrationActivity.this);
+
+                    builder.setTitle("Delete " + manager.getStore_name())
+                            .setMessage("Are you sure you want to delete it?")
+                            .setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    //deleteHero(hero.getId());
+                                }
+                            })
+                            .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+
+                                }
+                            })
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .show();
+                }
+            });
+
+            return listViewItem;
+        }
+    }
+
+    //region noNeed
+    //this method is actually fetching the json string
+    private void getJSON(final String urlWebService) {
+        /*
+         * As fetching the json string is a network operation
+         * And we cannot perform a network operation in main thread
+         * so we need an AsyncTask
+         * The constrains defined here are
+         * Void -> We are not passing anything
+         * Void -> Nothing at progress update as well
+         * String -> After completion it should return a string and it will be the json string
+         * */
+        class GetJSON extends AsyncTask<Void, Void, String> {
+
+            //this method will be called before execution
+            //you can display a progress bar or something
+            //so that user can understand that he should wait
+            //as network operation may take some time
+            @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+            }
+
+            //this method will be called after execution
+            //so here we are displaying a toast with the json string
+            @Override
+            protected void onPostExecute(String s) {
+                super.onPostExecute(s);
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+            }
+
+            //in this method we are fetching the json string
+            @Override
+            protected String doInBackground(Void... voids) {
+                try {
+                    //creating a URL
+                    URL url = new URL(urlWebService);
+
+                    //Opening the URL using HttpURLConnection
+                    HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+                    //StringBuilder object to read the string from the service
+                    StringBuilder sb = new StringBuilder();
+
+                    //We will use a buffered reader to read the string from service
+                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                    //A simple string to read values from each line
+                    String json;
+
+                    //reading until we don't find null
+                    while ((json = bufferedReader.readLine()) != null) {
+
+                        //appending it to string builder
+                        sb.append(json + "\n");
+                    }
+
+                    //finally returning the read string
+                    return sb.toString().trim();
+                } catch (Exception e) {
+                    return null;
+                }
+
+            }
+        }
+
+        //creating asynctask object and executing it
+        GetJSON getJSON = new GetJSON();
+        getJSON.execute();
+    }
+    //endregion
+}
